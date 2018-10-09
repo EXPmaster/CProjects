23d95ee6cae7b87d687a77babb101d135d46714b
Leo <851268460@qq.com>
2018-10-09 12:10:46 +0800

    homework of the first week 1

---
+++ homework1_2_1/cmake-build-debug/CMakeFiles/homework1_2_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework1_2_1.dir/main.cpp.obj"
+  "homework1_2_1.pdb"
+  "homework1_2_1.exe"
+  "homework1_2_1.exe.manifest"
+  "libhomework1_2_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework1_2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
